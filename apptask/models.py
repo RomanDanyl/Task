ca145515354ff202b941293ebb5130d570cfc953
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-08-01 16:04:55 +0300

    Create models, make migrations, add database to .gitignore

--- apptask/models.py
+++ apptask/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=66)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
